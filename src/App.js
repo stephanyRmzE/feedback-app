902a245bb62994ff56c108bd4809806a2d85f4e3
stephany ramirez <stephany_re@hotmail.com>
2022-04-19 15:49:43 -0700

    link to about and homepage

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import FeedbackList from "./components/FeedbackList"
 import FeedbackData from "./data/FeedbackData"
 import FeedbackStats from './components/FeedbackStats'
 import FeedbackForm from './components/FeedbackForm'
+import AboutIconLink from "./components/AboutIconLink"
 import AboutPage from './pages/AboutPage'
 
 function App() {
@@ -37,6 +38,7 @@ function App() {
         </Route>
         <Route path='/about' element={<AboutPage/>} />
         </Routes>
+        <AboutIconLink/>
       </div>
     </Router>
   )
